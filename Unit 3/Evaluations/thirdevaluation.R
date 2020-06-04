94c84ba33b2a2c55deb83d9f269de70d3817124e
René <diaz.de.sandi.7@gmail.com>
2020-06-03 23:24:25 -0700

    Update thirdevaluation.R

--- Unit 3/Evaluations/thirdevaluation.R
+++ Unit 3/Evaluations/thirdevaluation.R
@@ -1,1 +1,18 @@
-{\rtf1}
+# Package for Naive Bayes
+library('e1071')
+library('caret')
+library('ggplot2')
+library('lattice')
+# CSV import
+socialnetwork <- read.csv(file.choose())
+
+set.seed(2018)
+t.ids <- createDataPartition(socialnetwork$Purchased, p = 0.67, list = F)
+
+model <- naiveBayes(Purchased ~., data = socialnetwork[t.ids,])
+model
+
+#Prediction
+
+pred <- predict(model, socialnetwork[-t.ids,])
+tab <- table(socialnetwork[-t.ids,]$Purchased, pred, dnn = c("Actual","Predicha"))

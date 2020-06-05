90cc8f45327fea0805868d5390b4c41c8520d019
René <diaz.de.sandi.7@gmail.com>
2020-06-05 03:53:58 -0700

    third evaluation finished
    A lot of pain
    
    Co-Authored-By: rexfor <rexfor@users.noreply.github.com>

--- Unit 3/thirdevaluation.R
+++ Unit 3/thirdevaluation.R
@@ -1,43 +1,88 @@
+# Clean Workspace or Ctrl + Shift + F10
+rm(list = ls())
+
 # Package for Naive Bayes
 library('e1071')
-library('ggplot2')
-library('lattice')
-library('caret')
+library('ggplot2') #necessary for caret
+library('lattice')  #necessary for caret
+library('caTools')
 
 # CSV import
-socialnetwork <- read.csv(file.choose())
+socialnetwork <- read.csv("C:/Users/rjds_/Desktop/Social_Network_Ads.csv")
 
 # Seed
 set.seed(4)
 
+# We don't need all the data
+socialnwrk <- socialnetwork[3:5]
 
-# We don't need the user id
-socialnwrk <- socialnetwork[,c(2,3,4,5)]
+# Purchased column as factor
+socialnwrk$Purchased = factor(socialnwrk$Purchased, levels = c(0,1))
+
+# Remove csv
+rm(socialnetwork)
 
 # Check all True
-# complete.cases(socialnwrk)
+#complete.cases(socialnwrk)
 
-# Training and test data
-ind <- sample(2,nrow(socialnwrk), replace = TRUE, prob = c(0.7,0.3))
+# Data split
+split = sample.split(socialnwrk$Purchased, SplitRatio = 0.7)
 
-# Training data
-trainData<- socialnwrk[ind==1,]
+# Training Data
+traindata = subset(socialnwrk, split == TRUE)
+testdata = subset(socialnwrk, split == FALSE)
 
-#Testing data
-testData<- socialnwrk[ind==2,]
+# Remove split
+rm(split)
 
+# Scaling
+traindata[-2] = scale(traindata[-2])
+testdata[-2] = scale(testdata[-2])
 
 # Naive Bayes Model
-model <- naiveBayes(as.factor(Purchased)~., data = socialnwrk)
+nbmodel <- naiveBayes(Purchased ~ Age, data = socialnwrk)
 
 # Check
-model
+nbmodel
 
-# Prediction 
-pred <- predict(model,testData)
+# Prediction
+library('caret')
+pred <- predict(nbmodel,testdata)
 
 # Table 
-tab <- table(testData$Purchased, pred, dnn = c("Actual", "Predicha"))
+tab <- table(testdata$Purchased, pred, dnn = c("Actual", "Predicha"))
 
 # Final Matrix
 confusionMatrix(tab)
+
+# Visualizing the Training set results
+library(ElemStatLearn)
+set = traindata
+X1 = seq(min(set[, 1]) - 0.5, max(set[, 1]) + 0.5)
+X2 = seq(min(set[, 2]) - 0.5, max(set[, 2]) + 0.5)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(nbmodel, newdata = grid_set,)
+plot(set[, -2],
+     main = 'Naive Bayes (Training set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+# Visualizing the Test set results
+library(ElemStatLearn)
+set = testdata
+X1 = seq(min(set[, 1]) - 0.5, max(set[, 1]) + 0.5)
+X2 = seq(min(set[, 2]) - 0.5, max(set[, 2]) + 0.5)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(nbmodel, newdata = grid_set)
+plot(set[, -3],
+     main = 'Naive Bayes (Test set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
